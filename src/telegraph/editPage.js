839847652b8add96d7e7bc1c0cda3ccf84873757
Sunny /dx/ <sunnyredmi5a@gmail.com>
2021-09-26 23:36:21 +0530

    removed: unwanted console.log

--- src/telegraph/editPage.js
+++ src/telegraph/editPage.js
@@ -4,10 +4,9 @@ const editPage = async data => {
 	try {
 		const url = 'https://api.telegra.ph/editPage';
 		const result = await axios.post(url, data);
-		console.log(result.data);
 		return result.data;
 	} catch (error) {
-		console.log(error.response.data);
+		console.log(error);
 	}
 };
 export default editPage;
