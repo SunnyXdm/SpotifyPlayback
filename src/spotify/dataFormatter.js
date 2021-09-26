036c6d7dc8378bfed47238f1f542851750c18c50
Sunny /dx/ <sunnyredmi5a@gmail.com>
2021-09-26 23:09:07 +0530

    made Spotify dataFormatter better & added preview_url

--- src/spotify/dataFormatter.js
+++ src/spotify/dataFormatter.js
@@ -1,34 +1,23 @@
 const dataFormatter = (user, data, error) => {
 	if (data === null) {
-		return {
-			user_id: user.user_id,
-			name: user.name,
-			message_id: user.message_id,
-			status: null
-		};
+		user.status = null;
+		return user;
 	} else if (error) {
-		return {
-			user_id: user.user_id,
-			name: user.name,
-			message_id: user.message_id,
-			status: 'error',
-			error: data
-		};
+		user.status = 'error';
+		user.error = data;
+		return user;
 	} else {
-		return {
-			user_id: user.user_id,
-			name: user.name,
-			message_id: user.message_id,
-			status: {
-				song: data.data.item.name,
-				song_link: data.data.item.external_urls.spotify,
-				artist: data.data.item.artists[0].name,
-				artist_link: data.data.item.artists[0].external_urls.spotify,
-				album_art: data.data.item.album.images[0].url
-				duration: data.data.item.duration_ms,
-				progress: data.data.progress_ms
-			}
+		user.status = {
+			song: data.data.item.name,
+			song_link: data.data.item.external_urls.spotify,
+			artist: data.data.item.artists[0].name,
+			artist_link: data.data.item.artists[0].external_urls.spotify,
+			album_art: data.data.item.album.images[0].url,
+			preview_url: data.data.item.preview_url,
+			duration: data.data.item.duration_ms,
+			progress: data.data.progress_ms
 		};
+		return user;
 	}
 };
 export default dataFormatter;
